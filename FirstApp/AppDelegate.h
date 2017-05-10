af7de5c159137e2f9b35379f9a83610eed1ea4b3
Nagaraju <nagarajus.dev@gmail.com>
2017-05-10 15:22:27 +0530

    First Commit

---
+++ FirstApp/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  FirstApp
+//
+//  Created by Aurigo Software on 5/9/17.
+//  Copyright © 2017 Apptarix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController   *navigationController;
+
+
+@end
+
---
+++ FirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApp
+//
+//  Created by Aurigo Software on 5/9/17.
+//  Copyright © 2017 Apptarix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
